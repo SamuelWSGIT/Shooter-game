463a029d525afcdb253b78c28d709934c2e47848
SamuelWSGIT <88410126+SamuelWSGIT@users.noreply.github.com>
2022-05-24 23:42:22 -0300

    Jogo da Cobrinha aula 4
    ja foi criado o ambiente, a cobrinha, o movimento e a função de movimento infinito da esquerda pra direita e de cima pra baixo continuo.

---
+++ style/script.js
@@ -0,0 +1,62 @@
+let canvas = document.getElementById('snake');
+let context = canvas.getContext('2d');
+let box = 32;
+let snake = [];
+snake[0]= {
+    x: 8 * box,
+    y: 8 * box
+}
+
+let direction = "right";
+
+function criarBG() {
+    context.fillStyle = "#022601";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha(){
+    for (i=0; i < snake.length; i++){
+        context.fillStyle = "#09A603";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction ="left";
+    if(event.keyCode == 38 && direction != "down") direction ="up";
+    if(event.keyCode == 39 && direction != "left") direction ="right";
+    if(event.keyCode == 40 && direction != "up") direction ="down";
+}
+
+function iniciarJogo(){
+
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x =0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x =16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y =0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction== "right") snakeX += box;
+    if(direction== "left") snakeX -= box;
+    if(direction== "up") snakeY -= box;
+    if(direction== "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
